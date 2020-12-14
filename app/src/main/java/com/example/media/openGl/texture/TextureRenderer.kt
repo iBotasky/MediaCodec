e1b61703672979e68e21c5fbf26a169427f27058
suliangbo <suliangbo@vv.cn>
2020-12-14 17:48:21 +0800

    Add texture render .

--- app/src/main/java/com/example/media/openGl/texture/TextureRenderer.kt
+++ app/src/main/java/com/example/media/openGl/texture/TextureRenderer.kt
@@ -5,6 +5,7 @@ import android.opengl.GLES20
 import android.opengl.GLES20.glClearColor
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
+import android.util.Log
 import com.example.media.R
 import com.example.media.openGl.utils.Constants
 import com.example.media.openGl.utils.ShaderHelper
@@ -20,9 +21,9 @@ class TextureRenderer(val mContext: Context) : GLSurfaceView.Renderer {
         const val U_VIEW_MATRIX = "u_ViewMatrix"
         const val U_MODEL_MATRIX = "u_ModelMatrix"
         const val U_PROJECTION_MATRIX = "u_ProjectionMatrix"
-        const val A_POSITION = "a_Position"
-        const val A_TEXTURE_COORDINATE = "a_TextureCoordinates"
-        const val U_TEXTURE_UNIT = "u_TextureUnit"
+        const val A_POSITION = "aPosition"
+        const val A_TEXTURE_COORDINATE = "aCoordinate"
+        const val U_TEXTURE_UNIT = "uTexture"
     }
 
     private var uModeMatrixAttr: Int = 0
@@ -39,40 +40,37 @@ class TextureRenderer(val mContext: Context) : GLSurfaceView.Renderer {
     protected var mvpMatrix = FloatArray(16)
 
     private val vertexArrayData = floatArrayOf(
-        0.5f, 0f,
-        0f, 1.0f,
-        1.0f, 1.0f
+        -1f, -1f,
+        1f, -1f,
+        -1f, 1f,
+        1f, 1f
     )
 
     private val textureArrayData = floatArrayOf(
-        0.5f, 0f,
-        0f, 1.0f,
-        1.0f, 1.0f
+        0f, 1f,
+        1f, 1f,
+        0f, 0f,
+        1f, 0f
     )
 
     private val verticesShader =
         """
-            attribute vec4 a_Position; 
-            attribute vec2 a_TextureCoordinates;
-            varying vec2 v_TextureCoordinates; 
-            uniform mat4 u_ModelMatrix;
-            uniform mat4 u_ViewMatrix;
-            uniform mat4 u_ProjectionMatrix;
-            uniform mat4 u_Matrix;
+            attribute vec4 aPosition; 
+            attribute vec2 aCoordinate;
+            varying vec2 vCoordinate;
             void main() {
-                v_TextureCoordinates = a_TextureCoordinates ;
-                gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;
+                vCoordinate = aCoordinate ;
+                gl_Position = aPosition;
             }
         """.trimIndent()
 
     private val fragmentShader =
         """
             precision mediump float;
-            uniform sampler2D u_TextureUnit;
-            varying vec2 v_TextureCoordinates;
+            uniform sampler2D uTexture;
+            varying vec2 vCoordinate;
             void main(){
-
-                gl_FragColor = texture2D(u_TextureUnit,v_TextureCoordinates);
+                gl_FragColor = texture2D(uTexture,vCoordinate);
             }
         """.trimIndent()
 
@@ -94,10 +92,9 @@ class TextureRenderer(val mContext: Context) : GLSurfaceView.Renderer {
     private var mProgram: Int = -1
 
     override fun onDrawFrame(gl: GL10?) {
+        Log.e("TextureRender", " onDrawFrame")
         GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT or GLES20.GL_COLOR_BUFFER_BIT)
-        GLES20.glUniformMatrix4fv(uModeMatrixAttr, 1, false, modelMatrix, 0)
-        GLES20.glUniformMatrix4fv(uViewMatrixAttr, 1, false, viewMatrix, 0)
-        GLES20.glUniformMatrix4fv(uProjectionMatrixAttr, 1, false, projectionMatrix, 0)
+
 
 
         GLES20.glVertexAttribPointer(aPositionAttr, 2, GLES20.GL_FLOAT, false, 0, vertexFloatBuffer)
@@ -114,45 +111,33 @@ class TextureRenderer(val mContext: Context) : GLSurfaceView.Renderer {
 
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextureId)
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3)
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4)
 
 
     }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        Log.e("TextureRender", " onSurfaceChange")
         GLES20.glViewport(0, 0, width, height)
-        val aspectRatio =
-            if (width > height) width.toFloat() / height.toFloat() else height.toFloat() / width.toFloat()
-        if (width > height) {
-            Matrix.orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f)
-        } else {
-            Matrix.orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f)
-        }
     }
 
     override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
         mProgram = ShaderHelper.buildProgram(verticesShader, fragmentShader)
+        Log.e("TextureRender", " onSurfaceCreated:$mProgram")
         glClearColor(.5f, .5f, .5f, 1f)
         // 获取各个参数句柄
         aPositionAttr = GLES20.glGetAttribLocation(mProgram, A_POSITION)
-        uModeMatrixAttr = GLES20.glGetUniformLocation(mProgram, U_MODEL_MATRIX)
-        uViewMatrixAttr = GLES20.glGetUniformLocation(mProgram, U_VIEW_MATRIX)
-        uProjectionMatrixAttr = GLES20.glGetUniformLocation(mProgram, U_PROJECTION_MATRIX)
 
         aTextureCoordinateAttr = GLES20.glGetAttribLocation(mProgram, A_TEXTURE_COORDINATE)
         uTextureUnitAttr = GLES20.glGetUniformLocation(mProgram, U_TEXTURE_UNIT)
 
-        mTextureId = TextureHelper.loadTexture(mContext, R.mipmap.ic_launcher)
+        mTextureId = TextureHelper.loadTexture(mContext, R.drawable.yellow)
 
         GLES20.glUniform1i(uTextureUnitAttr, 0)
-        val intBuffer = IntBuffer.allocate(1)
-        GLES20.glGetIntegerv(GLES20.GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS, intBuffer)
-        Matrix.setIdentityM(modelMatrix, 0)
+//        val intBuffer = IntBuffer.allocate(1)
+//        GLES20.glGetIntegerv(GLES20.GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS, intBuffer)
 
-//        Matrix.translateM(modelMatrix, 0, 0f, 0.5f, 0f)
-
-        Matrix.setIdentityM(viewMatrix, 0)
-        Matrix.setIdentityM(projectionMatrix, 0)
+        GLES20.glUseProgram(mProgram)
     }
 
 }
